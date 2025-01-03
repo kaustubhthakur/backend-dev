3dfff2ca275344c692329866a1664eabd9a985f3
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-04 01:31:43 +0530

    second commit

--- api/controllers/books.js
+++ api/controllers/books.js
@@ -1,13 +1,26 @@
 const Book = require('../models/Book')
-const createBook= async(req,res)=> {
-
-}
-const updateBook = async(req,res)=> {
-
-}
-const deleteBook = async(req,res)=> {
-
-}
-const getBook = async(req,res)=> {
+const createBook = async (req, res) => 
+{
+try {
+    const newbook = req.body;
+    const savebook = await Book.create(newbook);
     
+} catch (error) {
+    console.error(error);
+}
+}
+const updateBook = async (req, res) => {
+
+}
+const deleteBook = async (req, res) => {
+try {
+    const curid = req.params.id;
+    const deletebook = await Book.findByIdAndDelete(curid);
+
+} catch (error) {
+    
+}
+}
+const getBook = async (req, res) => {
+
 }
