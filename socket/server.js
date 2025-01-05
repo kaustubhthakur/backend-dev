34c4b79e3068dcde9d860d803ff9db8696de6aaa
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-06 01:13:59 +0530

    second commit

---
+++ socket/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const port = 9000;
+const socket = require('socket.io')
+const http = require('http')
+
+
+const server = http.createServer(app);
+const io = socket(server)
+app.use(express.static('public'))
+server.listen(port, () => {
+    console.log(`server is running on port ${port}...`)
+})
+io.on("connection",(socket)=> {
+    console.log('user is connected...');
+})
