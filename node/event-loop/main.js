f8e7ddaad65ff300d7aadb4bdb678740566571cb
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-07 00:22:45 +0530

    first commit

--- node/event-loop/main.js
+++ node/event-loop/main.js
@@ -3,4 +3,8 @@ const crypto= require("crypto")
 console.log('ddifjsdiofuiosfser')
 setTimeout(()=> {
     console.log('fdijfid')
-},[2000])
+},5000)
+fs.readFile(__filename,() => {
+    console.log('relrjhew');
+})
+crypto.pbkdf2()
