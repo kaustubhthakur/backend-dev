1565d0a2d4942cfc7e1aa21cf303513fea160be4
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-07 00:36:46 +0530

    second commit

--- node/streams.js
+++ node/streams.js
@@ -1,2 +1,17 @@
 //read data from the source or help to write data
 //readable,writable,duplex,transform
+const fs = require('fs')
+const d = require('zlib')
+const crypto = require('crypto')
+const {Transform} = require('stream')
+class Encrypt extends Transform{
+    constructor(key,vector)
+    {
+        super()
+        this.key = key
+        this.vector = vector
+    }
+    __transform(chunk,encoding,callback){
+        
+    }
+}
